3b876608c3c280e079b74ba8b5d902700ed086e1
torastrengel <hekuttaja@gmail.com>
2022-05-20 10:45:34 +1000

    Switch a custom button component to styled component

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import Button from '../button/button.component';
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 import FormInput from '../form-input/form-input.component';
 
 import {
@@ -76,7 +76,11 @@ const SignInForm = () => {
         />
         <Buttons>
           <Button type="submit">Sign In</Button>
-          <Button type="button" buttonType="google" onClick={signInWithGoogle}>
+          <Button
+            type="button"
+            buttonType={BUTTON_TYPE_CLASSES.google}
+            onClick={signInWithGoogle}
+          >
             Google sign in
           </Button>
         </Buttons>

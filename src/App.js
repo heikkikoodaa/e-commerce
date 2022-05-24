82f9531af257e4ffa7748e1b5eb10ea769ba2331
torastrengel <hekuttaja@gmail.com>
2022-05-24 13:09:03 +1000

    Move categories redux to shop and eradicate categories context

--- src/App.js
+++ src/App.js
@@ -5,11 +5,9 @@ import { useDispatch } from 'react-redux';
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
-  getCategoriesAndDocuments,
 } from './utils/firebase/firebase.utils';
 
 import { setCurrentUser } from './store/user/user.action';
-import { setCategoriesMap } from './store/categories/category.action';
 
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
@@ -30,14 +28,6 @@ const App = () => {
     });
 
     return unsubscribe;
-  }, []);
-
-  useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      dispatch(setCategoriesMap(categoryMap));
-    };
-    getCategoriesMap();
   }, []);
 
   return (
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,15 +1,16 @@
-import { useContext, useState, useEffect, Fragment } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
 
-import ProductCard from '../../components/product-card/product-card.component';
+import { useSelector } from 'react-redux';
+import { selectCategories } from '../../store/categories/category.selector';
 
-import { CategoriesContext } from '../../contexts/categories.context';
+import ProductCard from '../../components/product-card/product-card.component';
 
 import { CategoryContainer, CategoryTitle } from './category.styles';
 
 const Category = () => {
+  const categoriesMap = useSelector(selectCategories);
   const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {

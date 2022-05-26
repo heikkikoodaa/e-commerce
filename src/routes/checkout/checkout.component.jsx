3b53b362700cb805375b3a97c6a40770af4b381d
torastrengel <hekuttaja@gmail.com>
2022-05-26 11:59:23 +1000

    Remove unneeded useEffect

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,16 +1,13 @@
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 
 import { CartContext } from '../../contexts/cart.context';
+
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
 import { CheckoutContainer, Headers, TotalPrice } from './checkout.styles';
 
 const Checkout = () => {
-  const { cartItems, setIsCartOpen, cartTotal } = useContext(CartContext);
-
-  useEffect(() => {
-    setIsCartOpen(false);
-  }, [setIsCartOpen]);
+  const { cartItems, cartTotal } = useContext(CartContext);
 
   return (
     <CheckoutContainer>

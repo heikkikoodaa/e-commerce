c1645af016819c2a8d2efbf189bf43e649de0712
torastrengel <hekuttaja@gmail.com>
2022-05-27 11:14:58 +1000

    Add redux-thunk and implement loading spinner

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import thunk from 'redux-thunk';
 
 import { loggerMiddleware } from './middleware/logger';
 // import logger from 'redux-logger';
@@ -10,13 +11,14 @@ import { rootReducer } from './root-reducer';
 const persistConfig = {
   key: 'root',
   storage,
-  blacklist: ['user'],
+  whitelist: ['cart'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [
   process.env.NODE_ENV === 'development' && loggerMiddleware,
+  thunk,
 ].filter(Boolean);
 
 const composeEnhancer =
